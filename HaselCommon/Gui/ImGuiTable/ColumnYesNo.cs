7439fcc585612c0346777f45a34860c00f8dc088
Latihas <lyjp2002@163.com>
2026-03-29 14:38:05 +0800

    1.51.1.2

--- HaselCommon/Gui/ImGuiTable/ColumnYesNo.cs
+++ HaselCommon/Gui/ImGuiTable/ColumnYesNo.cs
@@ -3,5 +3,5 @@ namespace HaselCommon.Gui.ImGuiTable;
 public class ColumnYesNo<TRow> : ColumnBool<TRow>
 {
     public override string NameKeySpace => "ImGuiTable.ColumnYesNo";
-    public override string[] Names => ["No", "Yes"];
+    public override string[] Names => ["否", "是"];
 }
